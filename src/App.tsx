151e22fbc4e543cccd60cbe4f760ebe64be3ef9d
Fernando Paulino <xizes2@hotmail.com>
2022-08-20 14:07:10 +0200

    create register component and its styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import AppStyled from "./AppStyled";
+import Register from "./components/Register/Register";
 
-function App(): JSX.Element {
-  return <>Things</>;
-}
+const App = (): JSX.Element => {
+  return (
+    <>
+      <AppStyled className="main-container">
+        <h1 className="main-container__title">SociMonsters</h1>
+        <Register />
+      </AppStyled>
+    </>
+  );
+};
 
 export default App;
